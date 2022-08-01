982806ca0a6ae932e2ef4b1f23899b899300b5ac
pratham183 <68583795+pratham183@users.noreply.github.com>
2022-08-02 03:51:51 +0530

    first

---
+++ src/components/Contact.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="contact_container">
+        <div className="co_list">
+          <h1 className="ch">Let your customer get what they need</h1>
+          <p className="cp">
+            Supercharge your sales by becoming our merchant partner
+          </p>
+        </div>
+        <div>
+          <img src="assets/img/layer1.png" className="co_img" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
